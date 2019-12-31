02e3fdae864f963ddb4d8053282702dda5bb25a5
YaMing Wu <wuyaming@gmail.com>
2019-12-31 06:23:21 -0600

    Add Client and Server initial code.

---
+++ test/src/ClientTests.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file ClientTests.cpp
+ *
+ * This module contains the unit tests of the Http::Client class.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <Http/Client.hpp>
+
+TEST(ClientTests, Placeholder) {
+
+}
---
+++ test/src/ServerTests.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file ServerTests.cpp
+ *
+ * This module contains the unit tests of the Http::Server class.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <Http/Server.hpp>
+
+TEST(ServerTests, Placeholder) {
+
+}
